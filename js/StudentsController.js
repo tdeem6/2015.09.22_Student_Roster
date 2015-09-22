962b3d2e747f22fdbd356a8d3453c494e8b46aa4
Tyler Deem <tdeem6@gmail.com>
2015-09-22 07:54:02 -0700

    adds method 'addStudent' to StudentsController.js

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,9 +1,7 @@
 studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
-  $scope.students = [
-    { name: "Same Schmidt" },
-    { name: "Jessica Martin" },
-    { name: "Sandy Smith" },
-    { name: "Ryan Samuels" },
-    { name: "Brentwood Davis" },
-  ]
+  $scope.students = [];
+  $scope.addStudent = function() {
+    $scope.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  };
 });
