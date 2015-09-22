b37a726a79e6f7a4feaf5feb5884158ec06bbda6
Tyler Deem <tdeem6@gmail.com>
2015-09-22 08:30:03 -0700

    updates FieldTripsControllers.js

--- controllers/FieldTripsController.js
+++ controllers/FieldTripsController.js
@@ -1,5 +1,6 @@
-studentRoster.controller('FieldTripsCtrl', function FieldTripCtrl($scope) {
-  $scope.studentsWithPermission = function(student) {
+studentRoster.controller('FieldTripsCtrl', function FieldTripsCtrl($scope, StudentsFactory) {
+  $scope.students = StudentsFactory.students;
+  $scope.addStudentWithPermissionSlip = function(student) {
     student.permissionSlip = true;
   };
 });
